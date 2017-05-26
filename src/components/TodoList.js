b666c0c16f38edd5cfb314e6a61e72463480682b
Worapot Pengsuk <worapot@dek-d.com>
2017-05-26 17:38:05 +0700

    update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,45 +1,50 @@
 import TodoInput from './../containers/TodoInput'
 import TodoItem from './TodoItem'
 import { FILTER_ALL, FILTER_COMPLETE, FILTER_NON_COMPLETE } from './../constants/ActionTypes'
-export default (props) => (
-  <div>
-    <TodoInput />
-    {/* todo filter */}
-    <div>
-      <button onClick={(e) => {
-        e.preventDefault()
-        props.filterTodo(FILTER_ALL)
-      }}>FILTER_ALL</button>
-      <button onClick={(e) => {
-        e.preventDefault()
-        props.filterTodo(FILTER_COMPLETE)
-      }}>FILTER_COMPLETE</button>
-      <button onClick={(e) => {
-        e.preventDefault()
-        props.filterTodo(FILTER_NON_COMPLETE)
-      }}>FILTER_NON_COMPLETE</button>
-    </div>
-    {props.todos.length > 0
-      ? <ul>
-        {props.todos.map((todo, key) => {
-          if (props.filter === FILTER_COMPLETE && !todo.complete){
-            return null
-          } else if (props.filter === FILTER_NON_COMPLETE && todo.complete){
-            return null
-          } else {
-            return (
-              <TodoItem key={key} {...todo} onClickComplete={(e) => {
-                e.preventDefault()
-                props.todoCompleteToggle(todo.id)
-              }} onClickDelete={(e) => {
-                e.preventDefault()
-                props.deleteTodo(todo.id)
-              }} />
-            )
-          }
-        })}
-      </ul>
-      : <div>Empty</div>
+export default (props) => {
+  const todos = props.todos.filter((todo) => {
+    if (props.filter === FILTER_COMPLETE && !todo.complete) {
+      return false
+    } else if (props.filter === FILTER_NON_COMPLETE && todo.complete) {
+      return false
+    } else {
+      return true
     }
-  </div>
-)
+  })
+  const count = todos.length
+  return (
+    <div>
+      <TodoInput />
+      {/* todo filter */}
+      <div>
+        <button onClick={(e) => {
+          e.preventDefault()
+          props.filterTodo(FILTER_ALL)
+        }}>FILTER_ALL</button>
+        <button onClick={(e) => {
+          e.preventDefault()
+          props.filterTodo(FILTER_COMPLETE)
+        }}>FILTER_COMPLETE</button>
+        <button onClick={(e) => {
+          e.preventDefault()
+          props.filterTodo(FILTER_NON_COMPLETE)
+        }}>FILTER_NON_COMPLETE</button>
+      </div>
+      {props.todos.length > 0
+        ? <ul>
+          {todos.map((todo, key) => (
+            <TodoItem key={key} {...todo} onClickComplete={(e) => {
+              e.preventDefault()
+              props.todoCompleteToggle(todo.id)
+            }} onClickDelete={(e) => {
+              e.preventDefault()
+              props.deleteTodo(todo.id)
+            }} />
+          ))}
+        </ul>
+        : <div>Empty</div>
+      }
+      <span>total: {count}</span>
+    </div>
+  )
+}
