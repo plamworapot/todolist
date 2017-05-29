dbaa0fec67003bdd4555fa8d7574dd714fcf4e90
Worapot Pengsuk <worapot@dek-d.com>
2017-05-30 02:58:58 +0700

    Deploy b666c0c16f38edd5cfb314e6a61e72463480682b to GitHub pages [ci skip]

---
+++ _next/a7aad9b4-e5bb-4275-ae89-7988347a9a8a/page/_error/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports=webpackJsonp([9],{130:function(o,t,e){"use strict";o.exports=e(78)},281:function(o,t,e){o.exports=e(130)}},[281]);
+            return { page: comp.default }
+          })
+        
--- _next/b49852bf-4d8d-4585-96a7-458b2f71a2de/page/_error/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([6],{122:function(o,t,e){"use strict";o.exports=e(104)},272:function(o,t,e){o.exports=e(122)}},[272]);
-            return { page: comp.default }
-          })
-        
